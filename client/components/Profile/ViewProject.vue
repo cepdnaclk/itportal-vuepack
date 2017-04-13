29eba7f4a447c42a84d5597948f45a7fe61a7a78
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2017-04-13 12:22:59 +0530

    started with Project view components

---
+++ client/components/Profile/ViewProject.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="row">
+    <div class="col-md-2">
+        <img src="/img/project.png" class="img-thumbnail mx-auto align-content-center">
+    </div>
+    <div class="col-md-6">
+        <hr class="hidden-sm-up">
+        <h4 class="h4-responsive my-1 my-md-0">Project Title</h4>
+        <p class="text-truncate my-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae magnam a, amet id
+            minus libero repellendus, earum. Quidem ipsum veritatis voluptas sit natus autem,
+        hic cumque explicabo in, eius blanditiis.</p>
+        <div class="">
+            <div class="badge badge-success">Java</div>
+            <div class="badge badge-success">Java</div>
+            <div class="badge badge-success">Java</div>
+        </div>
+        <div class="with-icon d-flex my-1 text-uppercase small"><i class="material-icons responsive">code</i> Individual Project</div>
+    </div>
+    <div class="col-md-4">
+        <div class="actions d-flex justify-content-md-end  justify-content-sm-center">
+            <button class="btn btn-primary px-2"><i class="material-icons responsive">remove_red_eye</i></button>
+            <button class="btn btn-primary px-2"><i class="material-icons responsive">edit</i></button>
+            <button class="btn btn-primary px-2"><i class="material-icons responsive">link</i></button>
+        </div>
+    </div>
+</div>    
+</template>
+
