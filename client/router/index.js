ef901c22d951ba2a18dcbb0d446a01bac0089bd8
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2017-04-25 14:31:13 +0530

    added company dashboard

--- client/router/index.js
+++ client/router/index.js
@@ -22,6 +22,12 @@ import StudentDashboard_Tasks from '../views/Student/Components/Tasks'
 import StudentDashboard_EditProfile from '../views/Student/Components/EditProfile'
 import StudentDashboard_Settings from '../views/Student/Components/Settings'
 
+import OrganizationDashboard_Summary from '../views/Organization/Components/Summary'
+import OrganizationDashboard_Tasks from '../views/Organization/Components/Tasks'
+import OrganizationDashboard_EditProfile from '../views/Organization/Components/EditProfile'
+import OrganizationDashboard_EditCompany from '../views/Organization/Components/EditCompany'
+import OrganizationDashboard_Settings from '../views/Organization/Components/Settings'
+
 
 import GettingStarted from '../views/Misc/GettingStarted'
 import AllStudents from '../views/Misc/AllStudents'
@@ -142,7 +148,14 @@ var router = new Router({
             name: 'Organization_dashboard',
             meta: {
                 auth: true
-            }
+            },
+            children: [
+                {name: 'Organization_dashboard_summary', path: '', component: OrganizationDashboard_Summary },
+                {name: 'Organization_dashboard_tasks', path: 'tasks', component: OrganizationDashboard_Tasks },
+                {name: 'Organization_dashboard_editprofile', path: 'editprofile', component: OrganizationDashboard_EditProfile },
+                {name: 'Organization_dashboard_editcompany', path: 'editcompany', component: OrganizationDashboard_EditCompany },
+                {name: 'Organization_dashboard_settings', path: 'settings', component: OrganizationDashboard_Settings },
+            ]
         }, {
             path: '/staff',
             component: StaffDashboard,
--- client/views/Organization/Dashboard.vue
+++ client/views/Organization/Dashboard.vue
@@ -1,5 +1,56 @@
 <template>
-<div class="container">
-	<h1 class="h1-responsive">Organization Dashboard</h1>
-</div>
+    <div class="container">
+        <div class="row">
+            <div class="form col-md-12 dashboard-itportal">
+
+                <!-- Nav Tabs -->
+                <ul class="nav nav-tabs z-depth-1 mt-3" role="tablist">
+                    <li class="nav-item">
+                        <router-link :to="{name: 'Organization_dashboard_summary'}" exact class="nav-link with-icon flex-center"><i class="material-icons">multiline_chart</i>Dashboard</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{name: 'Organization_dashboard_tasks'}" class="nav-link with-icon flex-center"><i class="material-icons">list</i> Tasks</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{name: 'Organization_dashboard_editprofile'}" class="nav-link with-icon flex-center"><i class="material-icons">assignment_ind</i>My Profile</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{name: 'Organization_dashboard_editcompany'}" class="nav-link with-icon flex-center"><i class="material-icons">business</i>My Company</router-link>
+                    </li>
+                    <li class="nav-item">
+	                    <router-link :to="{name: 'Organization_dashboard_settings'}" class="nav-link with-icon flex-center"><i class="material-icons">settings</i>Settings</router-link>
+                    </li>
+                </ul>
+
+                <div class="tab-content card mb-3 child-view-dash-container">
+                	<transition name="slide-left" mode="out-in">
+                    <router-view class="child-view-dash"></router-view>
+                    </transition>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+	.child-view-dash-container {
+	  /*overflow-x: hidden;*/
+	  /*position: relative;*/
+
+	}
+	.child-view-dash {
+  position: relative;
+  transition: all .2s cubic-bezier(.55,0,.1,1);
+}
+.slide-left-enter, .slide-right-leave-active {
+  opacity: 0;
+  -webkit-transform: translate(30px, 0);
+  transform: translate(30px, 0);
+}
+.slide-left-leave-active, .slide-right-enter {
+  opacity: 0;
+  -webkit-transform: translate(-30px, 0);
+  transform: translate(-30px, 0);
+}
+</style>
