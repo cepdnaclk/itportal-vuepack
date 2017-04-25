0d51d81b22eef7558f259e9a27050e80bb188b42
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2017-04-25 16:16:38 +0530

    updated students view

--- client/views/Misc/AllStudents.vue
+++ client/views/Misc/AllStudents.vue
@@ -2,13 +2,15 @@
 	<div class="container">
 		<h1>All Students</h1>
 		<div class="row">
-			<div class="col-sm-4 rounded white p-2 d-flex" v-for="student in students">
-				<div class="pr-2">
-					<img class="rounded-circle" :src="student.StudentDetails.photo ? (restUrl + 'photo/user/large-' + student.StudentDetails.photo) : '/img/user.png'" style="height: 6em; border-radius: 6em" alt="" />
-				</div>
-				<div class="d-flex flex-column">
-					<span class="pt-1 student_name" >{{student.StudentDetails.name}}</span>
-					<span>{{student.StudentDetails.email}}</span>
+			<div class="col-sm-4 rounded" v-for="student in students">
+				<div class="white p-2 d-flex">
+					<div class="pr-2">
+						<img class="rounded-circle" :src="student.StudentDetails.photo ? (restUrl + 'photo/user/large-' + student.StudentDetails.photo) : '/img/user.png'" style="height: 6em; border-radius: 6em" alt="" />
+					</div>
+					<div class="d-flex flex-column">
+						<span class="pt-1 student_name" >{{student.StudentDetails.name}}</span>
+						<span>{{student.StudentDetails.email}}</span>
+					</div>
 				</div>
 
 			</div>	
