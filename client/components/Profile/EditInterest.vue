0f4441d424e4f6fcb90f08012610a51711eec4a9
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2017-06-07 00:06:17 +0530

    Updated profile links + interests

--- client/components/Profile/EditInterest.vue
+++ client/components/Profile/EditInterest.vue
@@ -3,25 +3,30 @@
     <form @submit.prevent="addFormData" v-show="isEditing">
         <hr>
         <div class="row">
-                <div class="col-md-6">
-                    <hr class="hidden-sm-up">
-                    <h4 class="h4-responsive my-1 my-md-0">
-                        <input type="text"
-                            placeholder="Interest Title"
-                            list="allInterestsList"
-                            v-model="interest.name">
-                        <datalist id="allInterestsList">
-                            <option v-for="interest in allInterests" :value="interest.name">{{interest.name}}</option>
-                        </datalist>
-                    </h4>
-                    <textarea placeholder="Description"  v-model="interest.description"></textarea>
+            <div class="col-md-6">
+                <hr class="hidden-sm-up">
+                <h4 class="h4-responsive my-1 my-md-0">
+
+                    <multiselect
+                      v-model="interest"
+                      :options="unselectedInterests"
+                      :taggable="true"
+                      @tag="addNewInterest"
+                      tag-placeholder="Add this as a new interest"
+                      placeholder="Add your interests"
+                      label="name"
+                      track-by="name"
+                    ></multiselect>
+                </h4>
+                <input v-if="new_newInterest" placeholder="Name of the interest"  v-model="interest.name">
+                <textarea v-if="new_newInterest" placeholder="Description"  v-model="interest.description"></textarea>
+            </div>
+            <div class="col-md-4">
+                <div class="actions d-flex justify-content-md-end  justify-content-sm-center">
+                    <button type="submit" class="btn btn-primary px-2 flex-center  with-icon"><i class="material-icons responsive">add</i> Add Interest</button>
+                    <button type="reset" @click.prevent="resetFormData" class="btn btn-primary px-2 flex-center  with-icon"><i class="material-icons responsive">undo</i> Reset</button>
                 </div>
-                <div class="col-md-4">
-                    <div class="actions d-flex justify-content-md-end  justify-content-sm-center">
-                        <button type="submit" class="btn btn-primary px-2 flex-center  with-icon"><i class="material-icons responsive">add</i> Add Interest</button>
-                        <button type="reset" @click.prevent="resetFormData" class="btn btn-primary px-2 flex-center  with-icon"><i class="material-icons responsive">undo</i> Reset</button>
-                    </div>
-                </div>
+            </div>
         </div>    
     </form>
 
@@ -36,10 +41,16 @@
 
 <script>
 import Vue from 'vue';
+import Multiselect from 'vue-multiselect'
 
 export default{
+    components: { Multiselect },
     props: {
         allInterests: {
+            type: Array,
+            required: true,
+        },
+        currentInterests: {
             type: Array,
             required: true,
         },
@@ -48,6 +59,7 @@ export default{
 
         return{
             isEditing: false,
+            new_newInterest: false,
             interest: {
                 name: '',
                 description: '',
@@ -55,31 +67,40 @@ export default{
         }
     },
      methods: {
+        addNewInterest: function(_new_interest){
+            this.interest.name = _new_interest;
+            this.interest.description = '';
+            this.new_newInterest = true;
+            console.log(this.interest);
+        },
         addFormData: function(){
             let vm = this;
 
-            let _alreadyInInterestsList = null;
+            if(this.new_newInterest){
+                if(this.interest.description == ''){
+                    console.log('empty value')
+                    return;
+                } else {
+                    Vue.rest.insertData(this.interest, 'interest', null, function(res){
+                        if(res){
+                            res.students.push('me');
+                            vm.$emit('interest-uploaded', res);
+                            vm.resetFormData();
+                            vm.isEditing = false;
+                            vm.addProfileToInterest(res._id);
+                        }
+                    });
+                }
+            } else {
 
-            _alreadyInInterestsList = _.find(vm.allInterests, function(o) {
-                return o.name == vm.interest.name;
-            });
-
-            if(_alreadyInInterestsList){
-                vm.addProfileToInterest(_alreadyInInterestsList._id);
-                vm.$emit('interest-uploaded', _alreadyInInterestsList);
-                    vm.isEditing = false;
-                
+                vm.addProfileToInterest(vm.interest._id);
+                vm.$emit('interest-uploaded', vm.interest);
+                vm.isEditing = false;
                 return;
             }
 
-            Vue.rest.insertData(this.interest, 'interest', null, function(res){
-                if(res){
-                    vm.$emit('interest-uploaded', res);
-                    vm.resetFormData();
-                    vm.isEditing = false;
-                    vm.addProfileToInterest(res._id);
-                }
-            });
+
+            
         },
         toggleEditing: function(){
             this.isEditing = !this.isEditing;
@@ -101,6 +122,12 @@ export default{
             .catch(function(res){
                 console.log(res);
             })
+        },
+    },
+    computed: {
+        unselectedInterests: function(){
+            // console.log();
+            return _.differenceBy(this.allInterests, this.currentInterests, '_id');
         }
     },
     watch: {
