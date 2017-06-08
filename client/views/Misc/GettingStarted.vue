304c46a9f0c6f0b4541952b2621708222b2a77e5
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2017-06-09 05:23:46 +0530

    Content Editor for Admin

--- client/views/Misc/GettingStarted.vue
+++ client/views/Misc/GettingStarted.vue
@@ -1,1 +1,26 @@
 <template src="../templates/gettingstarted.html"></template>
+<script>
+import Vue from 'vue'
+export default{
+	data: function(){
+		return {
+			content: ''
+		}
+	},
+	methods: {
+		getAllContent: function(){
+			let vm = this;
+			let _baseUrl = Vue.rest.restBaseUrl;
+
+			Vue.axios.get(_baseUrl + 'api/home/gettingStarted')
+			.then(function(res){
+				vm.content = res.data;
+			})
+			.catch(function(err){console.log(err)})
+		}
+	},
+	mounted: function(){
+		this.getAllContent();
+	}
+}
+</script>
