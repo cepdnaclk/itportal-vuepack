f81f56271e147086dc3e2879a9c77fe426e5c62c
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2017-06-09 08:06:29 +0530

    Functionality to Edit Content for Admin

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -14,17 +14,25 @@ export default {
 		        students: 0,
 		        companies: 0,
 		        students_selected: 0,
-		    }
+		    },
+		    content: {}
 		}
+	},
+	methods: {
+		moment: function(date){
+			return moment(date).format('LL');
+		},
 	},
 	mounted: function(){
 		let _baseUrl = Vue.rest.restBaseUrl;
 		let vm = this;
 
-		Vue.axios.get(_baseUrl + 'api/home/getCounts')
+		Vue.axios.get(_baseUrl + 'api/home/getData')
 		.then(function(res){
-			let _counts = res.data;
-			vm.counts = _counts;
+			let _data = res.data;
+			vm.counts = _data.count;
+			vm.content = _data.content;
+			console.log(_data);
 		})
 		.catch(function(err){console.log(err)})
 	}
