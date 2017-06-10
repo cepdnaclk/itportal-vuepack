8b2472c061d289d212cceffeddd35204b9344434
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2017-06-10 18:20:40 +0530

    added functionality to remove profile entities

--- client/components/Profile/ViewCocurricular.vue
+++ client/components/Profile/ViewCocurricular.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="row my-3">
+<div v-if="visible" class="row my-3">
     <div class="col-md-2">
         <img src="/img/extras.png" class="rounded-circle mx-auto align-content-center" style="width:100px; height: 100px;">
     </div>
@@ -10,15 +10,16 @@
     </div>
     <div class="col-md-4" v-if="isOnProfile">
         <div class="actions d-flex justify-content-md-end  justify-content-sm-center">
-            <button class="btn btn-primary px-2"><i class="material-icons responsive">remove_red_eye</i></button>
-            <button class="btn btn-primary px-2"><i class="material-icons responsive">edit</i></button>
-            <button class="btn btn-primary px-2"><i class="material-icons responsive">link</i></button>
+            <button v-show="false" lass="btn btn-primary px-2"><i class="material-icons responsive">edit</i></button>
+            <button class="btn btn-warning px-2" @click="deleteItem(cocurricular._id)"><i class="material-icons responsive">close</i></button>
         </div>
     </div>
 </div>    
 </template>
 
 <script>
+import Vue from 'vue';
+
 export default{
     
     props: {
@@ -27,6 +28,27 @@ export default{
         },
         isOnProfile: {
             default: false
+        }
+    },
+    data: function(){
+        return {
+            visible: true,
+        }
+    },
+    methods:{
+        deleteItem: function(_id){
+            let vm = this;
+            let _baseUrl = Vue.rest.restBaseUrl;
+
+            Vue.axios.delete(_baseUrl + 'api/profile/cocurricular/' + _id)
+            .then(function(res){
+                console.log(res);
+                vm.visible = false;
+            })
+            .catch(function(err){
+                console.log(err);
+            })
+
         }
     }
 }
