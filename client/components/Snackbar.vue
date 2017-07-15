833f254e7203e7aaf8950425bda4ba51f73ba5cd
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2017-07-15 09:13:21 +0530

    Added a loader to the snackbar

--- client/components/Snackbar.vue
+++ client/components/Snackbar.vue
@@ -2,14 +2,19 @@
 <transition name="slideup">
   
   <div v-show="state.snackbarVisible" class="message-wrapper" @click="hideSnackbar">
-    <span>{{state.snackbarMessage}}</span>
+
+    <transition name="appear">
+        <span v-show="state.snackbarLoaderVisible" class="loader-wrapper"><i class="material-icons responsive spinner">donut_small</i>  </span>
+    </transition>
+
+    <span class="message">{{state.snackbarMessage}}</span>
   </div>
 
 </transition>
 </template>
 
 <script>
-import Vue from 'vue'
+import Vue from 'vue' 
 
 export default {
   data(){
@@ -20,6 +25,7 @@ export default {
   methods: {
     hideSnackbar: function(){
       this.$store.commit('HIDE_MESSAGE');
+      this.$store.commit('HIDE_LOADER');
     }
   },
 }
@@ -47,5 +53,52 @@ export default {
   transition: all .3s ease;
   opacity: 0;
   transform: translate(-50%, -20px);
+}
+.appear-enter-active, .appear-leave-active {
+  transition: all .2s ease;
+  opacity: 1;
+  transform: scale(1,1);
+}
+.appear-enter, .appear-leave-active {
+  transition: all .3s ease;
+  opacity: 0;
+  transform: scale(0,0);
+}
+@keyframes loader-animation-1 {
+  from {transform: scale(0.9,1);}
+  to {transform: scale(1,0.9);}
+}
+@keyframes loader-animation-3 {
+  from {transform: scale(0.9,1.3);}
+  to {transform: scale(1.3,0.9);}
+}
+@keyframes loader-animation-2 {
+  from {transform: rotate(0deg);}
+  to {transform: rotate(360deg);}
+}
+@keyframes loader-rotation {
+  from {transform: rotate(0deg);}
+  to {transform: rotate(360deg);}
+}
+.spinner{
+  animation: loader-rotation 0.5s linear 0s infinite !important;
+  transform-origin: 15.5px 16px;
+  font-size: 32px !important;
+}
+.message-wrapper{
+  display: inline-flex;
+  align-content: center;
+  font-size: 16px !important;
+  height: 64px;
+}
+.message-wrapper .message{
+  line-height: 16px;
+  align-self: center;
+}
+.loader-wrapper i{
+  margin-right: 0px !important;
+}
+.loader-wrapper{
+  margin-right: 10px;
 }
 </style>
