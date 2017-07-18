bb85b7291e40acbb4d05238dc344475beb77fc1b
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2017-07-18 11:38:25 +0530

    Added list view functionality
    for all students and all organizations

--- client/views/Misc/AllOrganizations.vue
+++ client/views/Misc/AllOrganizations.vue
@@ -1,14 +1,50 @@
 <template>
 	<div class="container py-4">
-		<h1>All Organizations</h1>
 		<div class="row">
-			<div class="col-sm-4 rounded" v-for="organization in organizations_sorted">
+			<h1 class="col-md-6">
+				All Organizations
+			</h1>
+			<div class="col-md-6 text-right">
+				<div class="btn-group align-items-center">
+					<span class="mr-1">Sorted by {{(sort_by_alpha?"Alphabetic order": "Last Modified")}}</span>
+					<a class="btn btn-sm btn-primary" :class="{'active': sort_by_last_modified}" @click="setSort('last_modified')"><i class="material-icons">date_range</i></a>
+					<a class="btn btn-sm btn-primary" :class="{'active': sort_by_alpha}" @click="setSort('alpha')"><i class="material-icons">sort_by_alpha</i></a>
+				</div>
+				<div class="btn-group">
+					<a class="btn btn-sm btn-primary" :class="{'active': view_tiles}" @click="setView('tile')"><i class="material-icons">view_module</i></a>
+					<a class="btn btn-sm btn-primary" :class="{'active': view_list}" @click="setView('list')"><i class="material-icons">view_list</i></a>
+				</div>
+				
+			</div>
+		</div>
+		<br>
+		<div class="row">
+			<table class="table" v-show="view_list">
+				<tr v-for="organization in organizations_sorted" class="data-bar my-1">
+					<td>
+						<img class="rounded-circle" :src="organization.photo ? (restUrl + 'photo/organization/small-' + organization.photo) : '/img/company.png'" style="height: 3em; height: 3em; border-radius: 6em" alt="" />
+					</td>
+					<td>{{organization.name}}</td>
+					<td class="text-center">
+	                    <span v-if="organization.phone" class="d-flex align-content-center"><i class="material-icons responsive mr-1">call</i> {{organization.phone}}</span>
+                    </td>
+                    <td class="text-center">
+	                    <a class="btn btn-primary btn-rounded" target="_blank" v-if="organization.linksLinkedin" title="Linkedin" :href="preProcessURL(organization.linksLinkedin)"><i class="fa fa-linkedin"></i></a>
+	                    <a class="btn btn-primary btn-rounded" target="_blank" v-if="organization.linksGithub" title="Github" :href="preProcessURL(organization.linksGithub)"><i class="fa fa-github"></i></a>
+	                    <a class="btn btn-primary btn-rounded" target="_blank" v-if="organization.linkswebpage" title="Official Page" :href="preProcessURL(organization.linkswebpage)"><i class="fa fa-link"></i></a>
+	                    <a class="btn btn-primary btn-rounded" target="_blank" v-if="organization.linksFacebook" title="Facebook" :href="preProcessURL(organization.linksFacebook)"><i class="fa fa-facebook"></i></a>
+
+                    </td>
+					<td class="text-center"><router-link :to="'/profile/organization/' + organization._id" class="btn btn-primary btn-xsm">View Company Profile</router-link></td>
+				</tr>
+			</table>
+			<div class="col-sm-4 rounded" v-for="organization in organizations_sorted"  v-show="view_tiles">
 
 
 				<router-link :to="'/profile/organization/' + organization._id">
 					<div class="white p-2 d-flex mb-1">
 						<div class="pr-2">
-							<img class="rounded-circle" :src="organization.photo ? (restUrl + 'photo/organization/large-' + organization.photo) : '/img/user.png'" style="height: 6em; border-radius: 6em" alt="" />
+							<img class="rounded-circle" :src="organization.photo ? (restUrl + 'photo/organization/large-' + organization.photo) : '/img/company.png'" style="height: 6em; border-radius: 6em" alt="" />
 						</div>
 						<div class="d-flex flex-column">
 							<span class="pt-1 organization_name" >{{organization.name}}</span>
@@ -36,6 +72,13 @@
 	.container{
 		min-height: 70vh;
 	}
+	.data-bar{
+		background: #fff;
+		border-radius: 2px;
+	}
+	.data-bar td{
+		vertical-align: middle;
+	}
 </style>
 <script>
 	import Vue from 'vue';
@@ -47,6 +90,12 @@
 			return {
 				restUrl : config.url,
 				organizations: [],
+
+				sort_by_alpha: false,
+				sort_by_last_modified: true,
+				view_tiles: false,
+				view_list: true,
+
 			}
 		},
 		methods: {
@@ -63,13 +112,49 @@
 				});
 
 			},
+	        preProcessURL: function(url){
+	            if(url.indexOf('//')<0){
+	                return '//' + url;
+	            }
+	            return url;
+	        },
+	        setView: function(type){
+
+	        	if(type == 'tile'){
+	        		this.view_tiles = true;
+	        		this.view_list = false;
+	        	} else if(type == 'list') {
+	        		
+	        		this.view_tiles = false;
+	        		this.view_list = true;
+	        	}
+
+	        },
+	        setSort: function(type){
+	        	if(type == 'alpha'){
+	        		this.sort_by_alpha = true;
+	        		this.sort_by_last_modified = false;
+	        	} else if(type == 'last_modified') {
+
+	        		this.sort_by_alpha = false;
+	        		this.sort_by_last_modified = true;
+	        	}
+	        }
 		},
 		computed: {
 			organizations_sorted: function(){
 				let vm = this;
-				return _.sortBy(vm.organizations, function(o){
-					return - (new Date(o.updatedAt));
-				})
+				if(vm.sort_by_alpha == true){
+					return _.sortBy(vm.organizations, function(o){
+						return o.name;
+					})
+				}
+				if(vm.sort_by_last_modified == true){
+					return _.sortBy(vm.organizations, function(o){
+						return - (new Date(o.updatedAt));
+					})
+				}
+
 			},
 		},
 		created: function(){
--- client/views/Misc/AllStudents.vue
+++ client/views/Misc/AllStudents.vue
@@ -1,8 +1,43 @@
 <template>
-	<div class="container py-4">
-		<h1>All Students</h1>
+	<div class="container py-4"><div class="row">
+			<h1 class="col-md-6">
+				All Students
+			</h1>
+			<div class="col-md-6 text-right">
+				<div class="btn-group align-items-center">
+					<span class="mr-1">Sorted by {{(sort_by_alpha?"Alphabetic order": "Last Modified")}}</span>
+					<a class="btn btn-sm btn-primary" :class="{'active': sort_by_last_modified}" @click="setSort('last_modified')"><i class="material-icons">date_range</i></a>
+					<a class="btn btn-sm btn-primary" :class="{'active': sort_by_alpha}" @click="setSort('alpha')"><i class="material-icons">sort_by_alpha</i></a>
+				</div>
+				<div class="btn-group">
+					<a class="btn btn-sm btn-primary" :class="{'active': view_tiles}" @click="setView('tile')"><i class="material-icons">view_module</i></a>
+					<a class="btn btn-sm btn-primary" :class="{'active': view_list}" @click="setView('list')"><i class="material-icons">view_list</i></a>
+				</div>
+				
+			</div>
+		</div>
+		<br>
 		<div class="row">
-			<div class="col-sm-4 rounded" v-for="student in students_sorted">
+			<table class="table" v-show="view_list">
+				<tr v-for="student in students_sorted" class="data-bar my-1">
+					<td>
+						<img class="rounded-circle" :src="student.StudentDetails.photo ? (restUrl + 'photo/user/large-' + student.StudentDetails.photo) : '/img/user.png'" style="height: 3em; height: 3em; border-radius: 6em" alt="" />
+					</td>
+					<td>{{student.StudentDetails.name}}</td>
+					<td class="text-center">
+	                    <span v-if="student.StudentDetails.phone" class="d-flex align-content-center"><i class="material-icons responsive mr-1">call</i> {{student.StudentDetails.phone}}</span>
+                    </td>
+                    <td class="text-center">
+	                    <a class="btn btn-primary btn-rounded btn-xsm" target="_blank" v-if="student.StudentDetails.linksLinkedin" title="Linkedin" :href="preProcessURL(student.StudentDetails.linksLinkedin)"><i class="fa fa-linkedin fa-small"></i></a>
+	                    <a class="btn btn-primary btn-rounded btn-xsm" target="_blank" v-if="student.StudentDetails.linksGithub" title="Github" :href="preProcessURL(student.StudentDetails.linksGithub)"><i class="fa fa-github fa-small"></i></a>
+	                    <a class="btn btn-primary btn-rounded btn-xsm" target="_blank" v-if="student.StudentDetails.linkswebpage" title="Official Page" :href="preProcessURL(student.StudentDetails.linkswebpage)"><i class="fa fa-link fa-small"></i></a>
+	                    <a class="btn btn-primary btn-rounded btn-xsm" target="_blank" v-if="student.StudentDetails.linksFacebook" title="Facebook" :href="preProcessURL(student.StudentDetails.linksFacebook)"><i class="fa fa-facebook fa-small"></i></a>
+
+                    </td>
+					<td class="text-center"><router-link :to="'/profile/student/' + student._id" class="btn btn-primary btn-xsm">View Student Profile</router-link></td>
+				</tr>
+			</table>
+			<div class="col-sm-4 rounded" v-for="student in students_sorted" v-show="view_tiles">
 
 
 			<router-link :to="'/profile/student/' + student._id">
@@ -28,6 +63,13 @@
 	.container{
 		min-height: 70vh;
 	}
+	.data-bar{
+		background: #fff;
+		border-radius: 2px;
+	}
+	.data-bar td{
+		vertical-align: middle;
+	}
 </style>
 <script>
 	import Vue from 'vue';
@@ -39,6 +81,11 @@
 			return {
 				restUrl : config.url,
 				students: [],
+
+				sort_by_alpha: false,
+				sort_by_last_modified: true,
+				view_tiles: false,
+				view_list: true,
 			}
 		},
 		methods: {
@@ -56,14 +103,49 @@
 
 				});
 
-			}
+			},
+	        preProcessURL: function(url){
+	            if(url.indexOf('//')<0){
+	                return '//' + url;
+	            }
+	            return url;
+	        },
+	        setView: function(type){
+
+	        	if(type == 'tile'){
+	        		this.view_tiles = true;
+	        		this.view_list = false;
+	        	} else if(type == 'list') {
+	        		
+	        		this.view_tiles = false;
+	        		this.view_list = true;
+	        	}
+
+	        },
+	        setSort: function(type){
+	        	if(type == 'alpha'){
+	        		this.sort_by_alpha = true;
+	        		this.sort_by_last_modified = false;
+	        	} else if(type == 'last_modified') {
+
+	        		this.sort_by_alpha = false;
+	        		this.sort_by_last_modified = true;
+	        	}
+	        }
 		},
 		computed: {
 			students_sorted: function(){
 				let vm = this;
-				return _.sortBy(vm.students, function(o){
-					return - (new Date(o.updatedAt));
-				})
+				if(vm.sort_by_alpha == true){
+					return _.sortBy(vm.students, function(o){
+						return o.registrationNumber;
+					})
+				}
+				if(vm.sort_by_last_modified == true){
+					return _.sortBy(vm.students, function(o){
+						return - (new Date(o.updatedAt));
+					})
+				}
 			},
 		},
 		created: function(){
